5c0a09803e9da3a7cb708980bb3c72519cc4577f
Evaldas Buinauskas <buinauskas.evaldas@gmail.com>
2018-12-15 14:35:10 +0200

    Worked out file size limit

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,4 +1,4 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 //TEST DB
 //mongodb://han_solo:chewbacca@ds011379.mlab.com:11379/mean-boilerplate
@@ -7,15 +7,10 @@ var mongoose = require('mongoose');
 //mongodb://han_solo:chewbacca@ds011419.mlab.com:11419/mean-boilerplate-test
 
 //Set up MongoDb connection
-function _init(){
-    try{
-        // return mongoose.createConnection('mongodb://han_solo:chewbacca@ds011379.mlab.com:11379/mean-boilerplate');
-        return mongoose.createConnection('mongodb://localhost:27017/mean-boilerplate');
-
-        //return mongoose.createConnection('mongodb://han_solo:chewbacca@ds011419.mlab.com:11419/mean-boilerplate-test');
-    }catch(err){
-        console.log("No internet connection :(");
-    }
-};
+function _init() {
+    return mongoose.createConnection(
+        'mongodb://evaldas:ujthUZDCr6BwkaXQ@development-shard-00-00-ogafb.gcp.mongodb.net:27017,development-shard-00-01-ogafb.gcp.mongodb.net:27017,development-shard-00-02-ogafb.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Development-shard-0&authSource=admin&retryWrites=true'
+    );
+}
 
 module.exports.init = _init;
--- routes/api/0.1/dronePartAPI.js
+++ routes/api/0.1/dronePartAPI.js
@@ -1,12 +1,29 @@
 const express = require('express');
-const router = express.Router();
 const domain = require('domain');
+const multer = require('multer');
 const fs = require('fs');
 const dronePartDAO = require('./../../../model/DAO/dronePartDAO');
-// multer  = require('multer');
+const router = express.Router();
+const uuid = require('uuid/v4');
 
-// var upload = multer({ dest: 'uploads/' });
+const upload = multer({ dest: 'uploads/' });
 
+router.post('/newPart', upload.array(), function(req, res) {
+    const base64Data = req.body.images[0];
+    console.log('writing file...', base64Data);
+
+    const imagePath = `/Temp/Upload/${uuid()}.png`;
+
+    fs.writeFile(imagePath, base64Data, 'base64', function(err) {
+        if (err) console.log(err);
+
+        fs.readFile(imagePath, function(err, data) {
+            if (err) throw err;
+            console.log('reading file...', data.toString('base64'));
+            res.send(data);
+        });
+    });
+});
 // var upload = multer({dest: 'uploads/',
 //     onFileUploadStart: function (file) {
 //       console.log(file.originalname + ' is starting ...')
@@ -14,189 +31,189 @@ const dronePartDAO = require('./../../../model/DAO/dronePartDAO');
 // });
 //CREATE a new dronePart
 //router.post('/newPart', upload.any(), function (req, res){
-router.post('/newPart', function(req, res) {
-    console.log('We are here');
-    // console.log(req);
-    // console.log(Object.keys(req.body.images));
+// router.post('/newPart', function(req, res) {
+//     console.log('We are here');
+//     // console.log(req);
+//     // console.log(Object.keys(req.body.images));
 
-    // cia failo info uzkoduota stringify reikia atkoduoti
-    // let imagee = req.body.images;//[0];
-    // console.info(req);
-    // console.info(JSON.parse(req.body.images));
-    // let imagee = JSON.parse(req.body.images);
-    // console.info(req.body.images[0]);
-    // let imagee = req.body.images[0];
-    console.info(req.body.images[0]);
-    const imagee = req.body.images[0];
-    // console.info(JSON.parse(req.body.images[0]));
-    // let imagee = JSON.parse(req.body.images[0]);
-    // console.info(JSON.parse(imagee));
-    // yra so failo duomenis i testas.png
-    // var buf = new Buffer(imagee);
-    // fs.writeFile('./testas/image.png', buf);
-    // fs.writeFile("./testas/testas.png", imagee, 'utf8', function(err) {
-    // const base64Data = imagee.replace(/^data:([A-Za-z-+/]+);base64,/, '');
-    // console.info(base64Data);
-    fs.writeFile('./testas/testas.png', imagee, 'base64', err => {
-        // fs.writeFile("./testas/testas.png", imagee.toString('utf8'), 'base64', (err) => {
-        // fs.writeFile("./testas/testas.png", imagee, 'utf8', (err) => {
+//     // cia failo info uzkoduota stringify reikia atkoduoti
+//     // let imagee = req.body.images;//[0];
+//     // console.info(req);
+//     // console.info(JSON.parse(req.body.images));
+//     // let imagee = JSON.parse(req.body.images);
+//     // console.info(req.body.images[0]);
+//     // let imagee = req.body.images[0];
+//     console.info(req.body.images[0]);
+//     const imagee = req.body.images[0];
+//     // console.info(JSON.parse(req.body.images[0]));
+//     // let imagee = JSON.parse(req.body.images[0]);
+//     // console.info(JSON.parse(imagee));
+//     // yra so failo duomenis i testas.png
+//     // var buf = new Buffer(imagee);
+//     // fs.writeFile('./testas/image.png', buf);
+//     // fs.writeFile("./testas/testas.png", imagee, 'utf8', function(err) {
+//     // const base64Data = imagee.replace(/^data:([A-Za-z-+/]+);base64,/, '');
+//     // console.info(base64Data);
+//     fs.writeFile('./testas/testas.png', imagee, 'base64', err => {
+//         // fs.writeFile("./testas/testas.png", imagee.toString('utf8'), 'base64', (err) => {
+//         // fs.writeFile("./testas/testas.png", imagee, 'utf8', (err) => {
 
-        // fs.writeFile("./testas/testas.png", imagee, function(err) {
-        if (err) {
-            console.info(err);
-            return;
-        }
+//         // fs.writeFile("./testas/testas.png", imagee, function(err) {
+//         if (err) {
+//             console.info(err);
+//             return;
+//         }
 
-        console.log('The file was saved!');
-    });
+//         console.log('The file was saved!');
+//     });
 
-    // console.log(JSON.parse(req.body.images)[0]);
-    // var storage = multer.diskStorage({ //multers disk storage settings
-    //     destination: function (req, file, cb) {
-    //         cb(null, '../documents/')
-    //     },
-    //     filename: function (req, file, cb) {
-    //         cb(null, file.originalname)
-    //     }
-    // });
-    console.log('We are here 2');
-    // var upload = multer({ //multer settings
-    //     storage: storage
-    // }).single('file');
+//     // console.log(JSON.parse(req.body.images)[0]);
+//     // var storage = multer.diskStorage({ //multers disk storage settings
+//     //     destination: function (req, file, cb) {
+//     //         cb(null, '../documents/')
+//     //     },
+//     //     filename: function (req, file, cb) {
+//     //         cb(null, file.originalname)
+//     //     }
+//     // });
+//     console.log('We are here 2');
+//     // var upload = multer({ //multer settings
+//     //     storage: storage
+//     // }).single('file');
 
-    // upload(req, res, function(err) {
-    //     if (err) {
-    //         res.json({ error_code: 1, err_desc: err });
-    //         return;
-    //     }
-    //     res.json({ error_code: 0, err_desc: null });
-    // });
+//     // upload(req, res, function(err) {
+//     //     if (err) {
+//     //         res.json({ error_code: 1, err_desc: err });
+//     //         return;
+//     //     }
+//     //     res.json({ error_code: 0, err_desc: null });
+//     // });
 
-    //})
-    //router.post('/newPart', upload.any(), function (req, res){
-    const d = domain.create();
+//     //})
+//     //router.post('/newPart', upload.any(), function (req, res){
+//     const d = domain.create();
 
-    console.info(req.files);
-    console.log(req.files);
+//     console.info(req.files);
+//     console.log(req.files);
 
-    d.on('error', function(error) {
-        console.log(error.stacktrace);
-        res.status(500).send({ error: error.message });
-    });
+//     d.on('error', function(error) {
+//         console.log(error.stacktrace);
+//         res.status(500).send({ error: error.message });
+//     });
 
-    console.log('Images RV');
-    console.info(req.files);
+//     console.log('Images RV');
+//     console.info(req.files);
 
-    d.run(function() {
-        console.log('Images RV 2');
-        const specs = {
-            weight: req.body.weight,
-            height: req.body.height,
-            width: req.body.width,
-            lenght: req.body.length,
-            mountWidth: req.body.mountWidth,
-            mountLength: req.body.mountLength,
-            voltsMin: req.body.voltsMin,
-            voltsMax: req.body.voltsMax,
-            voltMetric: req.body.voltMetric,
-            part4in1ESC: req.body.part4in1ESC,
-            ampsConstant: req.body.ampsConstant,
-            ampsPeak: req.body.ampsPeak,
-            firmware: req.body.firmware
-        };
+//     d.run(function() {
+//         console.log('Images RV 2');
+//         const specs = {
+//             weight: req.body.weight,
+//             height: req.body.height,
+//             width: req.body.width,
+//             lenght: req.body.length,
+//             mountWidth: req.body.mountWidth,
+//             mountLength: req.body.mountLength,
+//             voltsMin: req.body.voltsMin,
+//             voltsMax: req.body.voltsMax,
+//             voltMetric: req.body.voltMetric,
+//             part4in1ESC: req.body.part4in1ESC,
+//             ampsConstant: req.body.ampsConstant,
+//             ampsPeak: req.body.ampsPeak,
+//             firmware: req.body.firmware
+//         };
 
-        const supportedESCFirmware = {
-            BLHeli: req.body.BLHeli,
-            BLHeli_S: req.body.BLHeli_S,
-            BLHeli_32: req.body.BLHeli_32,
-            SimonK: req.body.SimonK,
-            KISS: req.body.KISS,
-            Other: req.body.OtherESCFirmware
-        };
+//         const supportedESCFirmware = {
+//             BLHeli: req.body.BLHeli,
+//             BLHeli_S: req.body.BLHeli_S,
+//             BLHeli_32: req.body.BLHeli_32,
+//             SimonK: req.body.SimonK,
+//             KISS: req.body.KISS,
+//             Other: req.body.OtherESCFirmware
+//         };
 
-        const supportedESCProtocol = {
-            PWM: req.body.PWM,
-            Oneshot125: req.body.Oneshot125,
-            Oneshot42: req.body.Oneshot42,
-            Multishot: req.body.Multishot,
-            Dshot150: req.body.Dshot150,
-            Dshot300: req.body.Dshot300,
-            Dshot600: req.body.Dshot600,
-            Dshot1200: req.body.Dshot1200,
-            ProShot: req.body.ProShot
-        };
+//         const supportedESCProtocol = {
+//             PWM: req.body.PWM,
+//             Oneshot125: req.body.Oneshot125,
+//             Oneshot42: req.body.Oneshot42,
+//             Multishot: req.body.Multishot,
+//             Dshot150: req.body.Dshot150,
+//             Dshot300: req.body.Dshot300,
+//             Dshot600: req.body.Dshot600,
+//             Dshot1200: req.body.Dshot1200,
+//             ProShot: req.body.ProShot
+//         };
 
-        const supportedFCPins = {
-            pinBEC33v: req.body.pinBEC33v,
-            pinBEC5v: req.body.pinBEC5v,
-            pinBEC9v: req.body.pinBEC9v,
-            pinVBat: req.body.pinVBat,
-            pinUARTS: req.body.pinUARTS,
-            pinCAM: req.body.pinCAM,
-            pinVTX: req.body.pinVTX,
-            pinBuzzer: req.body.pinBuzzer,
-            pinLED: req.body.pinLED
-        };
+//         const supportedFCPins = {
+//             pinBEC33v: req.body.pinBEC33v,
+//             pinBEC5v: req.body.pinBEC5v,
+//             pinBEC9v: req.body.pinBEC9v,
+//             pinVBat: req.body.pinVBat,
+//             pinUARTS: req.body.pinUARTS,
+//             pinCAM: req.body.pinCAM,
+//             pinVTX: req.body.pinVTX,
+//             pinBuzzer: req.body.pinBuzzer,
+//             pinLED: req.body.pinLED
+//         };
 
-        const features = {
-            supportedESCFirmware: supportedESCFirmware,
-            supportedESCProtocol: supportedESCProtocol,
-            supportedFCPins: supportedFCPins,
-            voltageMonitor: req.body.voltageMonitor,
-            currentMonitor: req.body.currentMonitor,
-            OSD: req.body.OSD,
-            blackbox: req.body.blackbox
-        };
+//         const features = {
+//             supportedESCFirmware: supportedESCFirmware,
+//             supportedESCProtocol: supportedESCProtocol,
+//             supportedFCPins: supportedFCPins,
+//             voltageMonitor: req.body.voltageMonitor,
+//             currentMonitor: req.body.currentMonitor,
+//             OSD: req.body.OSD,
+//             blackbox: req.body.blackbox
+//         };
 
-        const ratings = [];
+//         const ratings = [];
 
-        // const images = [];
-        // if (req.body.images){
-        //     for (var i in req.body.images) {
-        //         if (req.body.images.hasOwnProperty(i)) {
-        //             //var f = req.body.images[i].split(',');
-        //             images.add({
-        //                 title:            req.body.images[i].name,
-        //                 type:             req.body.images[i].type,
-        //                 image:            req.body.images[i].image
-        //                 // image: {
-        //                 //     data:         fs.readFileSync(req.body.images[i]),
-        //                 //     contentType:  'image/png'
-        //                 //     // contentType:  req.body.images[i].imageContentType
-        //                 // }
-        //             });
-        //         }
-        //     }
-        //}
+//         // const images = [];
+//         // if (req.body.images){
+//         //     for (var i in req.body.images) {
+//         //         if (req.body.images.hasOwnProperty(i)) {
+//         //             //var f = req.body.images[i].split(',');
+//         //             images.add({
+//         //                 title:            req.body.images[i].name,
+//         //                 type:             req.body.images[i].type,
+//         //                 image:            req.body.images[i].image
+//         //                 // image: {
+//         //                 //     data:         fs.readFileSync(req.body.images[i]),
+//         //                 //     contentType:  'image/png'
+//         //                 //     // contentType:  req.body.images[i].imageContentType
+//         //                 // }
+//         //             });
+//         //         }
+//         //     }
+//         //}
 
-        dronePartDAO.createDronePart(
-            {
-                name: req.body.name,
-                description: req.body.description,
-                category: req.body.category,
-                brand: req.body.brand,
-                company: req.body.company,
-                datePublished: req.body.datePublished,
-                specs: specs,
-                features: features,
-                // images:           images,
-                images: req.body.images,
-                ratings: ratings
-            },
-            {
-                success: function(f) {
-                    res.status(201).send({
-                        msg: 'dronePart created successfully: ' + req.body.name,
-                        data: f
-                    });
-                },
-                error: function(err) {
-                    res.status(500).send(err);
-                }
-            }
-        );
-    });
-});
+//         dronePartDAO.createDronePart(
+//             {
+//                 name: req.body.name,
+//                 description: req.body.description,
+//                 category: req.body.category,
+//                 brand: req.body.brand,
+//                 company: req.body.company,
+//                 datePublished: req.body.datePublished,
+//                 specs: specs,
+//                 features: features,
+//                 // images:           images,
+//                 images: req.body.images,
+//                 ratings: ratings
+//             },
+//             {
+//                 success: function(f) {
+//                     res.status(201).send({
+//                         msg: 'dronePart created successfully: ' + req.body.name,
+//                         data: f
+//                     });
+//                 },
+//                 error: function(err) {
+//                     res.status(500).send(err);
+//                 }
+//             }
+//         );
+//     });
+// });
 
 //READ all droneParts
 router.get('/', function(req, res) {
